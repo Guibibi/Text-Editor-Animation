bf4bc0e8dfa2d0ca302eb987157916687e0594b4
Guillaume Bedard <bgui34@gmail.com>
2019-01-09 03:58:04 -0500

    Fixed toggle for slider.

--- js/main.js
+++ js/main.js
@@ -2,9 +2,19 @@
 
 const colorbox = document.getElementById('color-circle');
 const inputs = [].slice.call(document.querySelectorAll('input'));
+let clicks = 0;
 
 $(document).ready(function () {
     $("#color-circle").click(function () {
-        $(".to-hide").toggleClass('hide');
+        if (clicks === 0) {
+            console.log("Hi")
+            $(".to-hide").addClass('hide');
+            clicks = 1;
+            $('.slider-container').css('visibility', 'visible');
+        } else {
+            $(".to-hide").removeClass('hide');
+            $('.slider-container').css('visibility', 'hidden');
+            clicks = 0;
+        }
     });
 });
